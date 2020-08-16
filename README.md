409fab98a14b39f935455953d2636ad6ad01702b
LOHITELECOM <s.lohidhasan071297@gmail.com>
2020-08-16 15:54:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-vt6ia2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-vt6ia2)
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  
+  return (
+   <div>
+     <div id="mySidenav" className="sidebar">
+     <div id="mySidenav" className="sidebar sidenav">
+    <a href="#home"><i className="fa fa-fw fa-home"></i> Home</a>
+     <a href="#services"><i className="fa fa-fw fa-wrench"></i> Product</a>
+       <a href="#clients"><i className="fa fa-fw fa-user"></i> Category</a>
+  <a href="#contact"><i className="fa fa-fw fa-envelope"></i> Contact</a>
+  <a href="#Logout"><i className="fa  fa-power-off"></i> LogOut</a>
+     </div>
+     </div>
+     
+  </div>
+  );
+
+}
